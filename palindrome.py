b2159f97a5724e33c12f7fd2ac723a04dc75177d
Gerryqo <gerass1396@yandex.ru>
2024-09-30 23:26:23 +0300

    Переработал код palindrome.py
    1) Установил точку входа в программу #1
    2) Обернул код в функции #2
    3) Упростил код #3

--- palindrome.py
+++ palindrome.py
@@ -1,24 +1,34 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--in_file', help='The path to the file with a possible list of palindromes',
-                    type=str, metavar='incoming.txt', required=True)
-parser.add_argument('-o', '--out_file', help='The path to the file with the filtered list of palindromes',
-                    type=str, metavar='outcoming.txt', required=True)
-args = parser.parse_args()
+
+def pars_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--in_file', help='The path to the file with a possible list of palindromes',
+                        type=str, metavar='incoming.txt', required=True)
+    parser.add_argument('-o', '--out_file', help='The path to the file with the filtered list of palindromes',
+                        type=str, metavar='outcoming.txt', required=True)
+    return parser.parse_args()
 
 
-def find_palindrome(raw_string):
-    if str(raw_string.lower()) == str(raw_string.lower())[::-1]:
-        return True
+def is_palindrome(raw_string):
+    return raw_string.lower() == raw_string.lower()[::-1]
 
 
-try:
-    with open(args.in_file, 'r', encoding='utf-8') as in_file:
-        with open(args.out_file, 'w', encoding='utf-8') as out_file:
-            for line in in_file.readlines():
-                if find_palindrome(line.rstrip()):
-                    out_file.write(line)
-    print('Done')
-except FileNotFoundError:
-    print('The incoming file was not found')
+def find_palindrome(in_path, out_path):
+    with open(in_path, 'r', encoding='utf-8') as in_file, open(out_path, 'w', encoding='utf-8') as out_file:
+        for line in in_file.readlines():
+            if is_palindrome(line.rstrip()):
+                out_file.write(line)
+
+
+def main():
+    try:
+        args = pars_args()
+        find_palindrome(args.in_file, args.out_file)
+        print('Done')
+    except FileNotFoundError:
+        print('The incoming file was not found')
+
+
+if __name__ == '__main__':
+    main()
